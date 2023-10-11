15169c2598b199f569ea16deafc303c8f130877a
Felipe Bassi <fkb5105@psu.edu>
2023-10-11 09:34:59 -0400

    querySelector button

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 window.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
-    alert ('Welcome to my JavaScript Experiments!');
+    alert('Welcome to my JavaScript Experiments!');
     var colorButton = document.getElementById("colorButton");
     var fontSizeButton = document.getElementById("fontSizeButton");
-    var resetButton = document.getElementById("resetButton"); 
-    var table = document.getElementById("myTable");
+    var resetButton = document.getElementById("resetButton");
+    var customButton = document.querySelector(".custom-button"); 
 
     colorButton.addEventListener('click', changeColor, false);
     fontSizeButton.addEventListener('click', increaseFontSize, false);
-    resetButton.addEventListener('click', resetStyles, false); 
+    resetButton.addEventListener('click', resetStyles, false);
+    customButton.addEventListener('click', customButtonClick, false); 
 }
 
 function changeColor() {
@@ -28,7 +29,17 @@ function increaseFontSize() {
 
 function resetStyles() {
     var table = document.getElementById("myTable");
-    table.style.backgroundColor = ""; 
-    table.style.color = ""; 
-    table.style.fontSize = ""; 
+    table.style.backgroundColor = "";
+    table.style.color = "";
+    table.style.fontSize = "";
+    var tableHeader = document.querySelector("thead");
+    tableHeader.style.color = ""; 
+    tableHeader.style.backgroundColor = ""
+}
+
+function customButtonClick() {
+    var tableHeader = document.querySelector("thead");
+    tableHeader.style.color = "white"; 
+    tableHeader.style.backgroundColor = "#009CDE";
+    console.log("Custom button clicked");
 }
